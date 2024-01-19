e624b48ae3f096b70a6fce5859769127653122dc
Shinas Shaji <shinasshaji12@outlook.com>
2024-01-19 16:20:19 +0100

    Starter code

--- particle_filter_localization/particle_filter_localization.py
+++ particle_filter_localization/particle_filter_localization.py
@@ -1,5 +1,28 @@
-def main():
-    print('Hi from particle_filter_localization.')
+import rclpy
+from rclpy.node import Node
+
+
+class ParticleFilterLocalization(Node):
+    def __init__(self):
+
+        # Setup subscribers and publishers
+        
+        return
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = ParticleFilterLocalization()
+    
+    try:
+        while rclpy.ok():
+            rclpy.spin_once(node)
+
+    except Exception as e:
+        print(e)
+
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
